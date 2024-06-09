4b4a06fef142adf97904676757370addd6780270
Bosheng Li <li2343@purdue.edu>
2024-06-08 17:13:27 -0700

    Update RayDataDefinations.hpp

--- include/lib/RayTracer/RayDataDefinations.hpp
+++ include/lib/RayTracer/RayDataDefinations.hpp
@@ -151,17 +151,17 @@ namespace EvoEngine {
 
             auto z = 1.f - triangleBarycentrics.x - triangleBarycentrics.y;
             if (triangleBarycentrics.x > z && triangleBarycentrics.x > triangleBarycentrics.y) {
-                hitInfo.m_color = vx.m_color;
-                hitInfo.m_data = glm::vec3(vx.m_vertexInfo1, vx.m_vertexInfo2, vx.m_vertexInfo3);
-                hitInfo.m_data2 = vx.m_vertexInfo4;
-            } else if (triangleBarycentrics.y > z) {
                 hitInfo.m_color = vy.m_color;
                 hitInfo.m_data = glm::vec3(vy.m_vertexInfo1, vy.m_vertexInfo2, vy.m_vertexInfo3);
                 hitInfo.m_data2 = vy.m_vertexInfo4;
-            } else {
+            } else if (triangleBarycentrics.y > z) {
                 hitInfo.m_color = vz.m_color;
                 hitInfo.m_data = glm::vec3(vz.m_vertexInfo1, vz.m_vertexInfo2, vz.m_vertexInfo3);
                 hitInfo.m_data2 = vz.m_vertexInfo4;
+            } else {
+                hitInfo.m_color = vx.m_color;
+                hitInfo.m_data = glm::vec3(vx.m_vertexInfo1, vx.m_vertexInfo2, vx.m_vertexInfo3);
+                hitInfo.m_data2 = vx.m_vertexInfo4;
             }
             return hitInfo;
         }
@@ -200,11 +200,11 @@ namespace EvoEngine {
                                       const glm::uvec3 &triangleIndices) const {
             auto z = 1.f - triangleBarycentrics.x - triangleBarycentrics.y;
             if (triangleBarycentrics.x > z && triangleBarycentrics.x > triangleBarycentrics.y) {
-                return m_vertices[triangleIndices.x].m_color;
-            } else if (triangleBarycentrics.y > z) {
                 return m_vertices[triangleIndices.y].m_color;
+            } else if (triangleBarycentrics.y > z) {
+                return m_vertices[triangleIndices.z].m_color;
             }
-            return m_vertices[triangleIndices.z].m_color;
+            return m_vertices[triangleIndices.x].m_color;
         }
 
         __device__ glm::vec3 GetTransformedNormal(const glm::mat4 &globalTransform, const float2 &triangleBarycentrics,
